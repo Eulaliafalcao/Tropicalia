9405198674517a197dc4f2df259dce5dc1f931e5
Eulaliafalcao <meulaliadeoliveirafalcao@gmail.com>
2025-05-29 10:46:33 -0300

    123

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const botaoDeAcessibilidade = document.getElementById ('botao-acessibilidade')
+        const botaoDeAcessibilidade = document.getElementById ('opcao-acessibilidade')
+
+        botaoDeAcessibilidade.addEventListener('click', acessibilidade) {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+                botaoDeAcessibilidade.classList.toggle('apresenta-lista')
+)}
+)}
